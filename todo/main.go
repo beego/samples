0c0854263b89be8e509d023aca68a29a622921eb
codejuan <xh@decbug.com>
2015-12-28 07:25:17 +0800

    register dababase & add add get/new routers

--- todo/main.go
+++ todo/main.go
@@ -3,11 +3,21 @@ package main
 import (
 	"github.com/astaxie/beego"
 	"github.com/beego/samples/todo/controllers"
+
+	"github.com/astaxie/beego/orm"
+	_ "github.com/go-sql-driver/mysql"
 )
+
+func init() {
+orm.RegisterDriver("mysql", orm.DR_MySQL)
+orm.RegisterDataBase("default", "mysql", "root:@/test?charset=utf8")
+}
 
 func main() {
 	beego.Router("/", &controllers.MainController{})
 	beego.Router("/task/", &controllers.TaskController{}, "get:ListTasks;post:NewTask")
 	beego.Router("/task/:id:int", &controllers.TaskController{}, "get:GetTask;put:UpdateTask")
+
+	beego.Router("/post/", &controllers.PostController{}, "get:GetAll;post:Post")
 	beego.Run()
 }
