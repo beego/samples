d033c9ede880bc8ac3dce4a4c2d289f9ec01aba4
Aditya Gangipamula <adgang@tuta.io>
2016-07-30 21:23:34 +0530

    analysis of chat with just pos tagger

--- analyser/read_table.go
+++ analyser/read_table.go
@@ -5,24 +5,49 @@ import (
 	"fmt"
 	"samples/WebIM/models"
 	"os"
+	"path/filepath"
 )
+
 func main() {
 	db, err := gorm.Open("mysql", "newuser:password@/mb?charset=utf8&parseTime=True&loc=Local")
 	fmt.Printf("%v\n", err)
 	fmt.Printf("%v\n", db)
 	fmt.Printf("%v\n", db.HasTable("events"))
-	var event models.Event
-	data := db.Find(&event)
-	rows, _ := data.Rows()
-	defer rows.Close()
-	for rows.Next() {
-		var content string
-    var user string
-		var Type int
-		var timestamp int64
-		fmt.Printf("%v\n", rows.Scan(&Type, &user, &timestamp, &content))
-		fmt.Printf("%v\n", user)
-		fmt.Printf("%v\n", Type)
-		fmt.Printf("%v\n", content)
+
+	var events []models.Event
+	db.Find(&events)
+	fmt.Printf("%v\n", events)
+	writeToFile(events)
+}
+
+func writeToFile(events []models.Event) {
+
+	var StanfordLibPath = "/Users/adi/Downloads/stanford-postagger-2015-12-09/"
+	mainJar  := filepath.Join(StanfordLibPath, "stanford-postagger.jar")
+	libs := filepath.Join(StanfordLibPath, "lib/*")
+	model := filepath.Join(StanfordLibPath, "models/english-bidirectional-distsim.tagger")
+	inputFile := "/tmp/batch-output.txt"
+	outputFile := "/tmp/batch-tagged-output.txt"
+	cmd := fmt.Sprintf("java -mx300m -classpath %v:%v edu.stanford.nlp.tagger.maxent.MaxentTagger -model %v -textFile %v > %v", mainJar, libs, model, inputFile, outputFile)
+	fmt.Printf("%v\n", cmd)
+
+	fo, err := os.Create(inputFile)
+	if err != nil {
+			panic(err)
 	}
+	// close fo on exit and check for its returned error
+	defer func() {
+			if err := fo.Close(); err != nil {
+					panic(err)
+			}
+	}()
+
+	for _, event := range events {
+				fmt.Printf("%v\n", event)
+
+		if _, err := fo.WriteString(event.User + "_USER " + event.Content + "\n"); err != nil {
+				panic(err)
+		}
+	}
+
 }
